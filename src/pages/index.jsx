222fa2806a487464dd0cf241e7e4141853b17854
Natanael Caetano <natacaet@gmail.com>
2022-11-29 14:29:20 -0300

    submit

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/link-passhref */
 import { FaArrowRight } from 'react-icons/fa'
 import { Container, Content, Infos, Name, Function, Intro, LinkProjects, Logo, Img } from '../../styles/indexStyle'
 import Link from 'next/link'
